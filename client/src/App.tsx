8621a241f386f689d510917017ea85888802336b
ugly_nixon <brokan.as@gmail.com>
2022-04-01 16:33:40 +0300

    start

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
-
-
+import React, {useContext, useEffect} from 'react';
+import {BrowserRouter} from 'react-router-dom'
+import AppRouter from "./components/AppRouter";
+import NavBar from "./components/nav/NavBar";
+import './styles/App.css'
+import {Context} from "./index";
 
 function App() {
+    const {userStore} = useContext(Context)
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            userStore.checkAuth()
+        }
+
+    }, [])
   return (
-    <div >
-      work
-    </div>
+      <div className="App">
+          <BrowserRouter>
+              <NavBar/>
+              <AppRouter/>
+          </BrowserRouter>
+      </div>
+
   );
 }
 
